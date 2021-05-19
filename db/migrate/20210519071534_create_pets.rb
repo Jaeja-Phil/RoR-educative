ad61a15df503a3ac2b6cf1b0a523a5fd24997ce8
Phillip Choi <phillip@Phillips-Mac-mini.local>
2021-05-19 18:33:01 +0900

    db:rollback --> db:migrate to add text column to Pets table

--- db/migrate/20210519071534_create_pets.rb
+++ db/migrate/20210519071534_create_pets.rb
@@ -3,6 +3,7 @@ class CreatePets < ActiveRecord::Migration[6.1]
     create_table :pets do |t|
       t.string :name
       t.string :age
+      t.text :description
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,7 @@ ActiveRecord::Schema.define(version: 2021_05_19_071534) do
   create_table "pets", force: :cascade do |t|
     t.string "name"
     t.string "age"
+    t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
