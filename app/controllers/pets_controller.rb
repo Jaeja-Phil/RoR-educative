a87c5f34f06a726020a2e805ffd0d044ff00b078
Phillip Choi <phillip@Phillips-Mac-mini.local>
2021-05-19 20:40:00 +0900

    add validation to the form

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -6,7 +6,11 @@ class PetsController < ApplicationController
   def show
     # check rails routes
     # pets#show requires :id params
-    @pet = Pet.find(params[ :id])
+    @pet = Pet.find(params[:id])
+  end
+
+  def new
+    @pet = Pet.new
   end
 
   def create
@@ -15,13 +19,30 @@ class PetsController < ApplicationController
     # :pet --> has attributes that we are interested in
     # due to built-in security features of rails, you must specify which param to permit
     # and they are name, age and description in this case
-    @pet = Pet.new(params.require( :pet).permit( :name, :age, :description))
-    @pet.save
-    # redict to the pets#show
-    # same as rediect_to pet_path(@pet)
-    redirect_to @pet
+    @pet = Pet.new(params.require(:pet).permit(:name, :age, :description))
+    if @pet.save
+      # redict to the pets#show
+      # same as rediect_to pet_path(@pet)
+      redirect_to @pet
+    else
+      # return the user back to the creation form.
+      render 'new'
+    end
+  end
+
+  def update
+    @pet = Pet.find(params[:id])
+    @pet.update(params.require(:pet).permit(:name, :age, :description))
   end
 
   def edit
+    @pet = Pet.find(params[:id])
+  end
+
+  def destroy
+    @pet = Pet.find(params[:id])
+    @pet.destroy
+
+    redirect_to pets_path
   end
 end
