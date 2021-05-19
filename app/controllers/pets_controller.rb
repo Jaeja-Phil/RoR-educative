60841daefa42e0e3d301c19e9b7466bae3dca035
Phillip Choi <phillip@Phillips-Mac-mini.local>
2021-05-19 19:58:13 +0900

    fix pets_controller to get @pet resource and show it on pets#show

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -4,9 +4,22 @@ class PetsController < ApplicationController
   end
 
   def show
+    # check rails routes
+    # pets#show requires :id params
+    @pet = Pet.find(params[ :id])
   end
 
-  def new
+  def create
+    # param returns ActionController::Parameters object
+    # represents the fields coming in from the forms
+    # :pet --> has attributes that we are interested in
+    # due to built-in security features of rails, you must specify which param to permit
+    # and they are name, age and description in this case
+    @pet = Pet.new(params.require( :pet).permit( :name, :age, :description))
+    @pet.save
+    # redict to the pets#show
+    # same as rediect_to pet_path(@pet)
+    redirect_to @pet
   end
 
   def edit
